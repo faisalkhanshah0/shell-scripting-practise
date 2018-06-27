e0664308146fdb92d98e512d950c1c3c5953164a
shah <c-shah.faisal@timesinternet.in>
2018-06-27 13:09:34 +0530

    functions with case and local and global scop vars

--- functioneg.sh
+++ functioneg.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 #functions examples
-
+something () {
+echo Hello $1
+return 5
+}
 print_something() {
     local varname=hello
     echo hello world i am coming from print function
     echo from inside of function $varname
 }
-
+something
 print_something
 print_something
 echo from outside func : $varname
